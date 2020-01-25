12058cf6808b90945450a393822e2819cac0fe24
Matthew Onghai <44452452+matthewonghai13@users.noreply.github.com>
2020-01-25 17:01:40 -0600

    added make checklist cells

--- FlightCheck/FlightCheck/FirstViewController.swift
+++ FlightCheck/FlightCheck/FirstViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // table view of list
+    @IBOutlet weak var checklistTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checklistTableView.delegate = self
+        checklistTableView.dataSource = self
+        checklistTableView.rowHeight = 60
     }
+    
+    var itemsToAdd: [String] = ["Jacket", "Toothbrush"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // return number of cells total
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsToAdd.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "checklistItem", for: indexPath) as! checkCell
+        cell.checkLabel.text = itemsToAdd[indexPath.row]
+        
+        return cell
+    }
+
 
 
 }
