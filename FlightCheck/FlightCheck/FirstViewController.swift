555541cc6714f052ceccf7174e4e5b6197fd747f
Matthew Onghai <44452452+matthewonghai13@users.noreply.github.com>
2020-01-25 15:34:48 -0600

    Initialized storyboards

---
+++ FlightCheck/FlightCheck/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FlightCheck
+//
+//  Created by Matthew Onghai on 1/25/20.
+//  Copyright © 2020 Matthew Onghai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlightCheck/FlightCheck/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FlightCheck
+//
+//  Created by Matthew Onghai on 1/25/20.
+//  Copyright © 2020 Matthew Onghai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
