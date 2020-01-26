03e1d3fbba6ce25871bda38a2839931116bddfb8
Matthew Onghai <44452452+matthewonghai13@users.noreply.github.com>
2020-01-26 10:48:21 -0600

    added ui for recommendation page

--- FlightCheck/FlightCheck/RecViewController.swift
+++ FlightCheck/FlightCheck/RecViewController.swift
@@ -15,6 +15,9 @@ class RecViewController: UIViewController {
         let defaults = UserDefaults.standard
         let importedArray = defaults.object(forKey: "queryArray") as? [String] ?? [String]()
         print(importedArray)
+        defaults.set(nil, forKey: "queryArray")
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
     }
     
     func showView(sender: AnyObject) {
