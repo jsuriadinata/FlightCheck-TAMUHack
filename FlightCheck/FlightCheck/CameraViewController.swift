1ec2866ba4172f2b1084a5ece0eb0b088c9f4514
Thuy <n.haithuy1999@gmail.com>
2020-01-25 19:07:15 -0600

    Added camera scan

--- FlightCheck/FlightCheck/CameraViewController.swift
+++ FlightCheck/FlightCheck/CameraViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVKit
 
 class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // start up the camera
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(.builtInDualCamera,
+        for: .video, position: .back) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        
+        // classify a frame
+        previewLayer.frame = view.frame
     }
 
 
