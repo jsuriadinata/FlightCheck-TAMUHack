73c50b67df516e85f360107ee78269c91b5fcd25
Matthew Onghai <44452452+matthewonghai13@users.noreply.github.com>
2020-01-25 21:23:56 -0600

    removed minus check on list

--- FlightCheck/FlightCheck/FirstViewController.swift
+++ FlightCheck/FlightCheck/FirstViewController.swift
@@ -51,11 +51,9 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         if cell.isChecked == false {
             cell.checkImage.image = UIImage(systemName: "checkmark.circle")
-            cell.checkImage.tintColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
             cell.isChecked = true
         } else {
-            cell.checkImage.image = UIImage(systemName: "minus.circle")
-            cell.checkImage.tintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            cell.checkImage.image = nil
             cell.isChecked = false
         }
     }
