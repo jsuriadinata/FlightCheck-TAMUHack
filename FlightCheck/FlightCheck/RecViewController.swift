555cc25a70f908aefafd62761377a829b4166bb7
Matthew Onghai <44452452+matthewonghai13@users.noreply.github.com>
2020-01-26 01:35:52 -0600

    added Rec view

---
+++ FlightCheck/FlightCheck/RecViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RecViewController.swift
+//  FlightCheck
+//
+//  Created by Matthew Onghai on 1/25/20.
+//  Copyright © 2020 Matthew Onghai. All rights reserved.
+//
+
+import UIKit
+
+class RecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func showView(sender: AnyObject) {
+        print("adfsfas")
+    }
+
+
+}
