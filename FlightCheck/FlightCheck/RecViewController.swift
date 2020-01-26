258ab89203f8f50b06d2d62be9568cb5a01d3f3d
Thuy <n.haithuy1999@gmail.com>
2020-01-26 11:17:30 -0600

    Basic javascript control on rec view

--- FlightCheck/FlightCheck/RecViewController.swift
+++ FlightCheck/FlightCheck/RecViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import JavaScriptCore
 
 class RecViewController: UIViewController {
+    
+    var jsContext: JSContext!
+    
+    func initializeJS() {
+        self.jsContext = JSContext()
+        
+        // Specify the path to the jssource.js file.
+        if let jsSourcePath = Bundle.main.path(forResource: "getFlightData", ofType: "js") {
+            do {
+                // Load its contents to a String variable.
+                let jsSourceContents = try String(contentsOfFile: jsSourcePath)
 
+                // Add the Javascript code that currently exists in the jsSourceContents to the Javascript Runtime through the jsContext object.
+                self.jsContext.evaluateScript(jsSourceContents)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getWeatherItems() {
+        if let functionFullname = self.jsContext.objectForKeyedSubscript("getMoreItems") {
+            if let res = functionFullname.call(withArguments: [1653, ["HI"]]) {
+                let itemsToAdd = res.toArray()
+                print(itemsToAdd)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let defaults = UserDefaults.standard
+        initializeJS();
+        getWeatherItems();
         let importedArray = defaults.object(forKey: "queryArray") as? [String] ?? [String]()
         print(importedArray)
         defaults.set(nil, forKey: "queryArray")
