5533e144e2c75f60985771650d0dee571fa5d55d
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-01-26 01:24:54 -0600

    modified default list

--- defaultList.js
+++ defaultList.js
@@ -1,6 +1,10 @@
-function test (myBool){
+// Testing javascript and swift
+
+// Default list for checklist in app
+// param myBool == true will return the list, else it will return a list with "fail" in it
+function test(myBool){
     if(myBool){
-        return ['brush', 'tooth brush', 'clothes', 'toothpaste'];
+        return ['hair brush', 'toothbrush', 'toothpaste', 'deoderant', 'wallet', 'earbuds', 'clothes'];
     }
     return ['fail'];
 }
